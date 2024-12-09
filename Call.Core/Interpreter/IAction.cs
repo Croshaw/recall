c96ee904976de17f697eae375e66732371d35d49
Croshaw <safixxkir@yandex.ru>
2024-12-10 01:38:17 +0500

    feat(core): prepare for syntaxer and interpreter
    Added a universal data type for further manipulation and some classes

---
+++ Call.Core/Interpreter/IAction.cs
@@ -0,0 +1,89 @@
+﻿namespace Call.Core.Interpreter;
+
+public class UnaryOperatorAction
+{
+    private readonly Func<UniValue, UniValue> _function;
+
+    public UnaryOperatorAction(string @operator)
+    {
+        switch (@operator)
+        {
+            case "not":
+                _function = value => !value;
+                break;
+        }
+    }
+
+    public UniValue Evaluate(UniValue value)
+    {
+        return _function(value);
+    }
+}
+
+public class OperatorAction
+{
+    private readonly Func<UniValue, UniValue, UniValue> _function = null!;
+
+    public OperatorAction(string @operator)
+    {
+        _function = @operator switch
+        {
+            "+" => (a, b) => a + b,
+            "-" => (a, b) => a - b,
+            "or" => (a, b) => a || b,
+            "*" => (a, b) => a * b,
+            "/" => (a, b) => a / b,
+            "and" => (a, b) => a && b,
+            ">" => (a, b) => a > b,
+            "<" => (a, b) => a < b,
+            "=" => (a, b) => a == b,
+            "<=" => (a, b) => a <= b,
+            ">=" => (a, b) => a >= b,
+            "<>" => (a, b) => a != b,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public UniValue Evaluate(UniValue x, UniValue y)
+    {
+        return _function(x, y);
+    }
+}
+
+public class AddressAction
+{
+    public enum AddressType
+    {
+        Value,
+        Instruction
+    }
+
+    public AddressType Type { get; }
+    public int Address { get; }
+
+    public AddressAction(AddressType type, int address)
+    {
+        Type = type;
+        Address = address;
+    }
+}
+
+public class SpecialAction
+{
+    public enum SpecialActionType
+    {
+        JumpIfFalse,
+        JumpIfTrue,
+        Jump,
+        Print,
+        Read,
+        Assign
+    }
+
+    public SpecialActionType Type { get; }
+
+    public SpecialAction(SpecialActionType type)
+    {
+        Type = type;
+    }
+}
---
+++ Call.Core/Interpreter/UniValue.cs
@@ -0,0 +1,218 @@
+﻿using System.Globalization;
+
+namespace Call.Core.Interpreter;
+
+public enum ValueKind
+{
+    Double,
+    Integer,
+    Boolean
+}
+
+public readonly struct UniValue
+{
+    private readonly double _raw;
+    public ValueKind Kind { get; }
+
+    public UniValue(ValueKind kind)
+    {
+        Kind = kind;
+        _raw = 0;
+    }
+
+    public UniValue(double raw)
+    {
+        Kind = ValueKind.Double;
+        _raw = raw;
+    }
+
+    public UniValue(int raw)
+    {
+        Kind = ValueKind.Integer;
+        _raw = raw;
+    }
+
+    public UniValue(bool raw)
+    {
+        Kind = ValueKind.Boolean;
+        _raw = raw ? 1 : 0;
+    }
+
+    public UniValue(double raw, ValueKind kind)
+    {
+        _raw = raw;
+        Kind = kind;
+    }
+
+    public UniValue(int raw, ValueKind kind)
+    {
+        _raw = raw;
+        Kind = kind;
+    }
+
+    public UniValue(bool raw, ValueKind kind)
+    {
+        Kind = kind;
+        _raw = raw ? 1 : 0;
+    }
+
+    public object GetValue()
+    {
+        return Kind switch
+        {
+            ValueKind.Double => _raw,
+            ValueKind.Integer => (int)_raw,
+            ValueKind.Boolean => _raw >= 1.0
+        };
+    }
+
+    public int GetIntValue()
+    {
+        return (int)_raw;
+    }
+
+    public bool GetBoolValue()
+    {
+        return _raw != 0;
+    }
+
+    public double GetDoubleValue()
+    {
+        return _raw;
+    }
+
+    public static UniValue operator +(UniValue left, UniValue right)
+    {
+        var kind = (ValueKind)Math.Min((short)left.Kind, (short)right.Kind);
+        return new UniValue(
+            left.Kind == ValueKind.Double
+                ? left.GetDoubleValue()
+                : left.GetIntValue() + (right.Kind == ValueKind.Double ? right.GetDoubleValue() : right.GetIntValue()),
+            kind);
+    }
+
+    public static UniValue operator -(UniValue left, UniValue right)
+    {
+        var kind = (ValueKind)Math.Min((short)left.Kind, (short)right.Kind);
+        return new UniValue(
+            left.Kind == ValueKind.Double
+                ? left.GetDoubleValue()
+                : left.GetIntValue() - (right.Kind == ValueKind.Double ? right.GetDoubleValue() : right.GetIntValue()),
+            kind);
+    }
+
+    public static UniValue operator *(UniValue left, UniValue right)
+    {
+        var kind = (ValueKind)Math.Min((short)left.Kind, (short)right.Kind);
+        return new UniValue(
+            left.Kind == ValueKind.Double
+                ? left.GetDoubleValue()
+                : left.GetIntValue() * (right.Kind == ValueKind.Double ? right.GetDoubleValue() : right.GetIntValue()),
+            kind);
+    }
+
+    public static UniValue operator /(UniValue left, UniValue right)
+    {
+        if (right._raw == 0)
+            throw new DivideByZeroException();
+        var kind = (ValueKind)Math.Min((short)left.Kind, (short)right.Kind);
+        return new UniValue(
+            left.Kind == ValueKind.Double
+                ? left.GetDoubleValue()
+                : left.GetIntValue() / (right.Kind == ValueKind.Double ? right.GetDoubleValue() : right.GetIntValue()),
+            kind);
+    }
+
+    public static bool operator ==(UniValue left, UniValue right)
+    {
+        return left.Kind == right.Kind && (left.Kind == ValueKind.Double
+            ? left.GetDoubleValue()
+            : left.GetIntValue()) == (right.Kind == ValueKind.Double
+            ? right.GetDoubleValue()
+            : right.GetIntValue());
+    }
+
+    public static bool operator !=(UniValue left, UniValue right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator >(UniValue left, UniValue right)
+    {
+        return (left.Kind == ValueKind.Double
+            ? left.GetDoubleValue()
+            : left.GetIntValue()) > (right.Kind == ValueKind.Double
+            ? right.GetDoubleValue()
+            : right.GetIntValue());
+    }
+
+    public static bool operator >=(UniValue left, UniValue right)
+    {
+        return (left.Kind == ValueKind.Double
+            ? left.GetDoubleValue()
+            : left.GetIntValue()) > (right.Kind == ValueKind.Double
+            ? right.GetDoubleValue()
+            : right.GetIntValue());
+    }
+
+    public static bool operator <(UniValue left, UniValue right)
+    {
+        return (left.Kind == ValueKind.Double
+            ? left.GetDoubleValue()
+            : left.GetIntValue()) < (right.Kind == ValueKind.Double
+            ? right.GetDoubleValue()
+            : right.GetIntValue());
+    }
+
+    public static bool operator <=(UniValue left, UniValue right)
+    {
+        return (left.Kind == ValueKind.Double
+            ? left.GetDoubleValue()
+            : left.GetIntValue()) <= (right.Kind == ValueKind.Double
+            ? right.GetDoubleValue()
+            : right.GetIntValue());
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is UniValue other && this == other;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_raw, Kind);
+    }
+
+
+    public static implicit operator UniValue(bool value)
+    {
+        return new UniValue(value);
+    }
+
+    public static implicit operator bool(UniValue value)
+    {
+        return value.GetBoolValue();
+    }
+
+    public static UniValue Parse(string value)
+    {
+        if (value == "true")
+            return true;
+        if (value == "false")
+            return false;
+        var last = value.Last();
+        switch (last)
+        {
+            case 'd':
+                return new UniValue(int.Parse(value.Substring(0, value.Length - 1)));
+            case 'h':
+                return new UniValue(Convert.ToInt32(value.Substring(0, value.Length - 1), 16));
+            case 'b':
+                return new UniValue(Convert.ToInt32(value.Substring(0, value.Length - 1), 2));
+            case 'o':
+                return new UniValue(Convert.ToInt32(value.Substring(0, value.Length - 1), 8));
+            default:
+                return new UniValue(double.Parse(value));
+        }
+    }
+}
