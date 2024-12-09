4be0e9deb190b1fa074066fffa9ef15c921b88cb
Croshaw <safixxkir@yandex.ru>
2024-12-10 01:35:50 +0500

    feat(core): added Console.cs
    To simulate a console

---
+++ Call.Core/Configuration/Console.cs
@@ -0,0 +1,22 @@
+﻿namespace Call.Core.Configuration;
+
+public class Console : IDisposable
+{
+    public TextWriter COUT { get; }
+    public TextWriter CER { get; }
+    public TextReader CIN { get; }
+
+    public Console(TextWriter cout, TextWriter cer, TextReader cin)
+    {
+        COUT = cout;
+        CER = cer;
+        CIN = cin;
+    }
+
+    public void Dispose()
+    {
+        COUT.Dispose();
+        CER.Dispose();
+        CIN.Dispose();
+    }
+}
