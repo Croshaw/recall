41669da16d53f3485549c1eb1d048a7327024f32
Croshaw <safixxkir@yandex.ru>
2024-12-10 01:35:01 +0500

    feat(core): added Position.cs and Error.cs
    Added struct for lexem position(also in future use for errors) and error helper to create string message

---
+++ Call.Core/Utilities/Error.cs
@@ -0,0 +1,25 @@
+﻿namespace Call.Core.Utilities;
+
+public enum ErrorType
+{
+    Unknown,
+    Redefinition,
+    Undeclared,
+    Expected
+}
+
+public static class Error
+{
+    public static string GetMesg(ErrorType errorType, string value, Position position)
+    {
+        return errorType switch
+        {
+            ErrorType.Unknown => "Unknown type name '" + value + "' at position " + position.ToShortString(),
+            ErrorType.Expected => "At position " + position.ToShortString() + " is expected \"" + value + "\"",
+            ErrorType.Undeclared => "Use of undeclared identifier '" + value + "' at position " +
+                                    position.ToShortString(),
+            ErrorType.Redefinition => "Redefinition of " + value + " at position " + position.ToShortString(),
+            _ => throw new ArgumentOutOfRangeException(nameof(errorType), errorType, null)
+        };
+    }
+}
---
+++ Call.Core/Utilities/Position.cs
@@ -0,0 +1,22 @@
+﻿namespace Call.Core.Utilities;
+
+public readonly struct Position
+{
+    public int Row { get; }
+    public int Column { get; }
+    public int Length { get; }
+    public int Absolute { get; }
+
+    public Position(int row, int column, int length, int absolute)
+    {
+        Row = row;
+        Column = column;
+        Length = length;
+        Absolute = absolute;
+    }
+
+    public string ToShortString()
+    {
+        return $"{Row}:{Column}";
+    }
+}
