b598ab03efb7b288962538d1c8ec427e0a08fd8f
Croshaw <safixxkir@yandex.ru>
2024-12-12 00:07:24 +0500

    style(gui): rename TextBoxStreamWriter.cs to TextBoxTextWriter.cs

---
+++ Call.GUI/TextBoxTextWriter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Call.GUI;
+
+public class TextBoxTextWriter : TextWriter
+{
+    private readonly RichTextBox _textBox;
+    private readonly Color _textColor;
+
+    public TextBoxTextWriter(RichTextBox textBox, Color textColor)
+    {
+        _textBox = textBox;
+        _textColor = textColor;
+    }
+
+    public override Encoding Encoding => Encoding.UTF8;
+
+    public override void Write(char value)
+    {
+        AppendText(value.ToString());
+    }
+
+    public override void Write(string? value)
+    {
+        AppendText(value);
+    }
+
+    private void AppendText(string? text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return;
+        // Потокобезопасное добавление текста с учетом цвета
+        _textBox.Invoke((MethodInvoker)(() =>
+        {
+            var start = _textBox.TextLength;
+            _textBox.AppendText(text);
+            var end = _textBox.TextLength;
+
+            // Применяем цвет к добавленному тексту
+            _textBox.Select(start, end - start);
+            _textBox.SelectionColor = _textColor;
+            _textBox.SelectionLength = 0;
+        }));
+    }
+}
